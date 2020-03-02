0ef4ca89d62e59c0ce2cece6dc0edd133d3fd703
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2020-03-02 11:17:27 +0300

    Replace ViewPropTypes with PropTypes.shape

--- src/Swiper.js
+++ src/Swiper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Animated, PanResponder, StyleSheet, View, ViewPropTypes } from 'react-native';
+import { Animated, PanResponder, StyleSheet, View } from 'react-native';
 
 import DefaultControls from './Controls';
 
@@ -304,10 +304,18 @@ Swiper.propTypes = {
   onIndexChanged: PropTypes.func,
 
   positionFixed: PropTypes.bool, // Fix safari vertical bounces
-  containerStyle: ViewPropTypes.style,
-  innerContainerStyle: ViewPropTypes.style,
-  swipeAreaStyle: ViewPropTypes.style,
-  slideWrapperStyle: ViewPropTypes.style,
+  containerStyle: PropTypes.shape({
+    style: PropTypes.any,
+  }),
+  innerContainerStyle: PropTypes.shape({
+    style: PropTypes.any,
+  }),
+  swipeAreaStyle: PropTypes.shape({
+    style: PropTypes.any,
+  }),
+  slideWrapperStyle: PropTypes.shape({
+    style: PropTypes.any,
+  }),
 
   controlsEnabled: PropTypes.bool,
   controlsProps: PropTypes.shape(DefaultControls.propTypes),
